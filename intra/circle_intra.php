794e80ab2564bafeabf8e74df253a239f1c2af14
art <pakkawat.kh@gmail.com>
2022-02-09 22:34:31 +0700

    แยกfolder ใหม่

---
+++ intra/circle_intra.php
@@ -0,0 +1,11 @@
+<?php
+// require_once('connection.php');s
+// $_POST = json_decode(file_get_contents("php://input"),true);
+// $economic = $_POST['economic'];
+// $year = $_POST['year'];
+// $type = $_POST['type'];
+// print_r($economic);
+// $result = rand(60,98);e
+echo "ART";
+echo $result;
+?>
