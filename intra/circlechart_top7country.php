d3f21332fbaa4b128ea143a4f81009f7604514f8
art <pakkawat.kh@gmail.com>
2022-02-10 11:58:19 +0700

    updated

---
+++ intra/circlechart_top7country.php
@@ -0,0 +1,27 @@
+<?php
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+$countryFullList=$_POST['countryFullList'];
+$input=$_POST['input'];
+$selected=$_POST['selected'];
+
+$result[0]['name']="China";
+$result[1]['name']="Vietnam";
+$result[2]['name']="Thailand";
+$result[3]['name']="South Korea";
+$result[4]['name']= $selected['label'];
+$result[5]['name']="Spain";
+$result[6]['name']="Mlaysia";
+$result[7]['name']="Peru";
+
+$result[0]['value']=.93;
+$result[1]['value']=.85;
+$result[2]['value']=.8;
+$result[3]['value']=.78;
+$result[4]['value']=.7;
+$result[5]['value']=.64;
+$result[6]['value']=.6;
+$result[7]['value']=.5;
+
+echo json_encode($result);
+?>
