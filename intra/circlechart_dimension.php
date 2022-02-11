5c26b3e5f33943cd1dccb149d9d692c4b1247d4a
ART <pakkawat.kh@gmail.com>
2022-02-12 03:17:09 +0700

    reorganize

---
+++ intra/circlechart_dimension.php
@@ -0,0 +1,26 @@
+<?php
+require_once('../connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+$countryFullList=$_POST['countryFullList'];
+$input=$_POST['input'];
+
+$result[0]['name']="Trade and Investment";
+$result[1]['name']="Digital Economy";
+$result[2]['name']="Financial";
+$result[3]['name']="Regional Value Chains";
+$result[4]['name']="Average Index";
+$result[5]['name']="Infrastructure";
+$result[6]['name']="Movement of People";
+$result[7]['name']="Regional Cooperation";
+
+$result[0]['value']=.91;
+$result[1]['value']=.90;
+$result[2]['value']=.83;
+$result[3]['value']=.73;
+$result[4]['value']=.71;
+$result[5]['value']=.62;
+$result[6]['value']=.6;
+$result[7]['value']=.4;
+
+echo json_encode($result);
+?>
--- main/period_start_end.php
+++ main/period_start_end.php
@@ -1,5 +1,5 @@
 <?php
-require_once('connection.php');
+require_once('../connection.php');
 // $_POST = json_decode(file_get_contents("php://input"),true);
 $result['start']= 2012;
 $result['end']=2020;
