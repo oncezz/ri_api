cc702ddd64efd13fc94eca28d9a29ad5a49a3fba
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-02-12 16:00:14 +0700

    data table

--- data_availablity/table_data.php
+++ data_availablity/table_data.php
@@ -1,23 +1,51 @@
 <?php
 require_once('../connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
-
-$report=$_POST['report'];
-$partner=$_POST['partner'];
-$dataBase=$_POST['dataBase'];
-$compareType=$_POST['compareType'];
-$disaggregation=$_POST['disaggregation'];
+$report=$_POST['report'];                   // country report
+$partner=$_POST['partner'];                 // country partner
+$dataBase=$_POST['dataBase'];               // database type  'digi'= DigiSRII  ,  'all'= ALL Data
+$compareType=$_POST['compareType'];         // 'group'= Economy group,  'specific'= report, partner ; when compare 'group' report=partner
+$disaggregation=$_POST['disaggregation'];   // 'pair'=Pair , 'dimension'=Dimension and indicator
 
 for($i=0;$i<sizeof($report);$i++){
 
     $result[$i]['label']=$report[$i]['label'];
     $result[$i]['iso']=$report[$i]['iso'];
+    for($k=0;$k<8;$k++){
     for($j=0;$j<sizeof($partner);$j++){
-        if($i==$j){
-            $result[$i]['data'][$j]=-1 ;
+        
+
+        if($report[$i]['iso']==$partner[$j]['iso']){
+            $result[$i]['data'][$k][$j]=-99 ;                   // return -  mean same country
         }
-       else{$result[$i]['data'][$j]= rand(40,120) ;}
+       else{
+        $result[$i]['data'][$k][$j]= rand(40,120) ;
+            if($result[$i]['data'][$k][$j]>115){
+                $result[$i]['data'][$k][$j]=0;                  // return  0 = No Data
+            }
+            else if($result[$i]['data'][$k][$j]>100){
+                $result[$i]['data'][$k][$j]=100;
+            }
+        }
+    
+    
     }
+}
+//////// avg at data[0][n]  of 7 ; n mean 7 dimension
+for($j=0;$j<sizeof($partner);$j++){
+   
+    if($result[$i]['data'][0][$j]>0){
+        $count=0;
+        $result[$i]['data'][0][$j]=0;
+    for($k=1;$k<8;$k++){
+        if($result[$i]['data'][$k][$j]!=0){
+        $result[$i]['data'][0][$j]+=$result[$i]['data'][$k][$j];
+        $count++;
+        }
+    }
+    $result[$i]['data'][0][$j]=round($result[$i]['data'][0][$j]/$count);
+}
+}
 }
 echo json_encode($result);
 ?>
