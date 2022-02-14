0964201e45d1cb2f9cde29389507f5f468d8fcbd
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-02-14 17:50:25 +0700

    report > 1 cal Total score

--- data_availablity/table_data.php
+++ data_availablity/table_data.php
@@ -11,40 +11,60 @@ for($i=0;$i<sizeof($report);$i++){
 
     $result[$i]['label']=$report[$i]['label'];
     $result[$i]['iso']=$report[$i]['iso'];
-    for($k=0;$k<8;$k++){
+    for($k=1;$k<8;$k++){
+        for($j=0;$j<sizeof($partner);$j++){
+            if($report[$i]['iso']==$partner[$j]['iso']){
+                $result[$i]['data'][$k][$j]=-99 ;                   // return -  mean same country
+            }
+            else{
+                $result[$i]['data'][$k][$j]= rand(40,120) ;
+                if($result[$i]['data'][$k][$j]>115){
+                    $result[$i]['data'][$k][$j]=0;                  // return  0 = No Data
+                }
+                else if($result[$i]['data'][$k][$j]>100){
+                    $result[$i]['data'][$k][$j]=100;
+                }
+            }
+        }
+    } //////// avg at data[0][n]  of 7 ; n mean 7 dimension
     for($j=0;$j<sizeof($partner);$j++){
-        
-
-        if($report[$i]['iso']==$partner[$j]['iso']){
-            $result[$i]['data'][$k][$j]=-99 ;                   // return -  mean same country
-        }
-       else{
-        $result[$i]['data'][$k][$j]= rand(40,120) ;
-            if($result[$i]['data'][$k][$j]>115){
-                $result[$i]['data'][$k][$j]=0;                  // return  0 = No Data
-            }
-            else if($result[$i]['data'][$k][$j]>100){
-                $result[$i]['data'][$k][$j]=100;
-            }
-        }
-    
-    
-    }
-}
-//////// avg at data[0][n]  of 7 ; n mean 7 dimension
-for($j=0;$j<sizeof($partner);$j++){
-   
-    if($result[$i]['data'][0][$j]>0){
         $count=0;
         $result[$i]['data'][0][$j]=0;
-    for($k=1;$k<8;$k++){
-        if($result[$i]['data'][$k][$j]!=0){
-        $result[$i]['data'][0][$j]+=$result[$i]['data'][$k][$j];
-        $count++;
+        for($k=1;$k<8;$k++){
+            if($result[$i]['data'][$k][$j]!=0){
+                $result[$i]['data'][0][$j]+=$result[$i]['data'][$k][$j];
+                $count++;
+                }
+        }
+        $result[$i]['data'][0][$j]=round($result[$i]['data'][0][$j]/$count);
+    } //end avg all dimension
+    for($k=0;$k<8;$k++){ ////// avg all partner at data[]
+        $result[$i]['data'][$k][sizeof($partner)]=0;
+        $count=0;
+        for($j=0;$j<sizeof($partner);$j++){
+            if($result[$i]['data'][$k][$j]>0){
+                $result[$i]['data'][$k][sizeof($partner)]+=$result[$i]['data'][$k][$j];    
+                $count++;
+            }
+        }
+        $result[$i]['data'][$k][sizeof($partner)]=round($result[$i]['data'][$k][sizeof($partner)]/$count);
+    }  /// end avg all partner
+} /// end for i all report
+
+///////// avg all data at end table @ result[report]
+if(sizeof($report)>1){
+$result[sizeof($report)]['label']='Total';
+$result[sizeof($report)]['iso']='Total';
+for($j=0;$j<=sizeof($partner);$j++){ // use <= cause  + end avg all partner
+    $count=0;
+    $result[sizeof($report)]['data'][0][$j]=0;
+    for($i=0;$i<sizeof($report);$i++){
+        if($result[$i]['data'][0][$j]>0){
+                $result[sizeof($report)]['data'][0][$j]+=$result[$i]['data'][0][$j];    
+                $count++;
         }
     }
-    $result[$i]['data'][0][$j]=round($result[$i]['data'][0][$j]/$count);
-}
+    $result[sizeof($report)]['data'][0][$j]=round( $result[sizeof($report)]['data'][0][$j]/$count);
 }
 }
 echo json_encode($result);
