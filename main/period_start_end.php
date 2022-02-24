42c6e45df0d6538d14c4156c61d00d9a3920b7a6
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-02-24 18:03:43 +0700

    edit little intra > by country > linechart

--- main/period_start_end.php
+++ main/period_start_end.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../connection.php');
 // $_POST = json_decode(file_get_contents("php://input"),true);
-$result['start']= 2012;
-$result['end']=2020;
+$result['start']= 2010;
+$result['end']=2019;
 echo json_encode($result);
 ?>
