a2f5840c22ea6e4a42bdc56754e988708de5de1c
ART <pakkawat.kh@gmail.com>
2022-03-21 19:13:18 +0700

    update

--- economy/circle_economy.php
+++ economy/circle_economy.php
@@ -2,9 +2,38 @@
 require_once('../connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 $partner = $_POST['partner'];
-$reporting = $_POST['reporting'];
-$input = $_POST['input'];
+$reporting=$_POST['reporting'];
 
-$result = rand(60,98);
-echo $result;
+// echo $reporting;
+// $economic[0] = "AFG";
+// $economic[1]="BTN";
+// $economic[2]="LAO";
+// $economic[3]="NPL";
+$type = $_POST['type'];
+// echo $type;
+// $type = "Sustatain";
+
+if($type == "Conventional"){
+    $table = "ri_intra_con";
+} else {
+    $table = "ri_intra_sus";
+}
+
+$result = $db->count($table,[
+    "reporting"=>$reporting,
+    "partner"=>$partner
+]);
+// echo $result;
+
+$total = 0;
+for($i=0;$i<sizeof($reporting);$i++){
+    for($j=0;$j<sizeof($partner);$j++){
+        if($reporting[$i]!=$partner[$j]){
+            $total++;
+        }
+    }
+}
+// echo $total;
+$percent = round($result/$total *100);
+echo $percent;
 ?>
--- intra/circle_intra.php
+++ intra/circle_intra.php
@@ -2,8 +2,26 @@
 require_once('../connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 $economic = $_POST['economic'];
-$year = $_POST['year'];
+// $economic[0] = "AFG";
+// $economic[1]="BTN";
+// $economic[2]="LAO";
+// $economic[3]="NPL";
 $type = $_POST['type'];
-$result = rand(60,98);
-echo $result;
+// echo $type;
+// $type = "Sustatain";
+// print_r($economic);
+if($type == "Conventional"){
+    $table = "ri_intra_con";
+} else {
+    $table = "ri_intra_sus";
+}
+
+$result = $db->count($table,[
+    "reporting"=>$economic,
+    "partner"=>$economic
+]);
+// echo $result;
+$total = sizeof($economic)* (sizeof($economic)-1);
+$percent = round($result/$total *100);
+echo $percent;
 ?>
