1a51342d472230504cc068512bfae1e553735a0a
ART <pakkawat.kh@gmail.com>
2022-03-22 20:04:23 +0700

    update

--- intra/fourbar_intra.php
+++ intra/fourbar_intra.php
@@ -6,6 +6,20 @@ $name=$_POST['name'];
 $year = $_POST['year'];
 $type = $_POST['type'];
 
+if($type == "Sustainable"){
+    $table = "ri_5bar_intra_sus";
+} else {
+    $table = "ri_5bar_intra_con";
+}
+
+$yourScoreAvg =$db-> avg($table,"score",[
+    "reporter" => $economic,
+    "partner" => $economic,
+    "year"=>$year
+]) ;
+
+
+
 $result[0]['name'] = 'China-Mongolia';
 $result[0]['value']= 0.91;
 $result[0]['own']= false;
@@ -16,11 +30,14 @@ $result[1]['own']= false;
 
 $result[2]['name'] = $name;
 $result[2]['value']= 0.74;
-$result[2]['own']= true;
+$result[2]['own']= false;
 
 $result[3]['name'] = 'Asia-Pacific';
 $result[3]['value']= 0.56;
 $result[3]['own']= false;
 
+$result[4]['name'] = $name;
+$result[4]['value']= round($yourScoreAvg,2);
+$result[4]['own']= true;
 echo json_encode($result);
 ?>
